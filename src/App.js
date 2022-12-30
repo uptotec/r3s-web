51c81d26e4ce11cc25ffb2a275c7fef4184c0879
uptotec <mahmoudashraf11179@gmail.com>
2022-12-30 05:58:10 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="row">
+      <div class="columnData"></div>
+      <div class="columnAlert"></div>
+    </div>
+  );
+}
+
+export default App;
