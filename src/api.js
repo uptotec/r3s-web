691034b76cc4cd9e6e4c9e56e57141dc4ba260e6
uptotec <mahmoudashraf11179@gmail.com>
2023-01-12 23:14:37 +0200

    api to remote host

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const httpURL = 'http://localhost:4000';
-export const wsURL = 'ws://localhost:4000';
+export const httpURL = 'https://sea-lion-app-869d5.ondigitalocean.app';
+export const wsURL = 'wss://sea-lion-app-869d5.ondigitalocean.app/';
 
 const getReadings = async (startDate, endDate) => {
-  const res = await axios.get('http://localhost:4000/getReadings', {
+  const res = await axios.get(`${httpURL}/getReadings`, {
     params: { s: startDate, e: endDate },
   });
 
